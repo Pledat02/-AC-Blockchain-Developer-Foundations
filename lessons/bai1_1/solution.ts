79112bf1ee947fb021c92c6acf417e315e725425
phamledat <95115578+Pledat02@users.noreply.github.com>
2025-09-09 15:45:39 +0700

    resolve bai1_1

--- lessons/bai1_1/solution.ts
+++ lessons/bai1_1/solution.ts
@@ -1,4 +1,4 @@
-
+import crypto from "crypto";
 export type Block = {
   index: number;
   timestamp: string;
@@ -6,8 +6,18 @@ export type Block = {
   previous_hash: string;
   current_hash: string;
 };
+function calculateHash(index: number, timestamp: string, transactions: any[], previous_hash: string): string {
+  const value = index + timestamp + JSON.stringify(transactions) + previous_hash;
+  return crypto.createHash('sha256').update(value).digest('hex');
+}
 
 // ✍️ TODO: Viết hàm tại đây
 export function isValidBlock(block: Block): boolean {
-  return false; // Chỉnh lại logic
+  const recalculated = calculateHash(
+    block.index,
+    block.timestamp,
+    block.transactions,
+    block.previous_hash
+  );
+  return recalculated === block.current_hash;
 }
